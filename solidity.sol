f3b6c40d4c777b63bd0ac848dfe757288f9e1c9f
AnmolPatwal <anmolpatwal04@gmail.com>
2024-06-12 21:19:07 +0530

    Create solidity.sol

---
+++ solidity.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.6.12 <0.9.0;
+
+contract MyToken{
+  // public variables
+  
+  string public tokenName = "META";
+  string public tokenAbbrv = "MTA";
+  uint public totalSupply = 0;
+
+  // mapping variable
+
+  mapping (address => uint) public balances;
+
+  // mint function
+
+  function mint (address _address, uint _value) public { 
+    totalSupply += _value;
+    balances[_address] += _value;
+  }
+
+  // burn function
+
+  function burn (address _address, uint _value) public { 
+    if (balances[_address] >= _value) {
+      totalSupply -= _value;
+      balances [_address]-_value;
+    }
+  }
+}
